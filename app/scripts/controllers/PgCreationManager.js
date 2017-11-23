6b5a8b2fd356683f14ff1e6fa55830d82d1526ad
Miroku87 <miroku_87@yahoo.it>
2017-11-24 00:15:37 +0100

    Completata registrazione personaggio

--- app/scripts/controllers/PgCreationManager.js
+++ app/scripts/controllers/PgCreationManager.js
@@ -40,6 +40,36 @@
             $("#inviaDati").click(this.inviaDati.bind(this));
         },
 
+        setClassList: function ()
+        {
+            var classe       = {};
+
+            for( var cc in this.classInfos.classi_civili )
+            {
+                classe = this.classInfos.classi_civili[cc];
+                if( classe.id_classe )
+                    $("#" + CIVILIAN_CLASS_LIST_ID)
+                        .append( "<li data-id=\"" + classe.id_classe + "\">" + classe.nome_classe + "</li>" );
+            }
+
+            for( var cc in this.classInfos.classi_militari )
+            {
+                classe = this.classInfos.classi_militari[cc];
+
+                if( classe.id_classe )
+                {
+                    var disabled = parseInt( cc ) % 2 !== 0 ? "disabled='disabled'" : "";
+                    //var disabled = classe.nome_classe.indexOf( MILITARY_ADVA_CLASS_LABEL ) !== -1 ?
+                    // "disabled='disabled'" : "";
+                    $("#" + MILITARY_CLASS_LIST_ID)
+                        .append("<li data-id=\"" + classe.id_classe + "\" data-costo='1' "+disabled+">" + classe.nome_classe + "</li>");
+                }
+            }
+
+            this.setupListSelect();
+            this.setupClassesMarket();
+        },
+
         nomeRequisito: function ( element )
         {
             var id_prerequisito = parseInt( element.attr("data-prerequisito"));
@@ -76,7 +106,7 @@
             return abilita.prerequisito_abilita === null || $( "#"+CIVILIAN_ABILITY_BUCKET_ID ).find("li[data-id='"+abilita.prerequisito_abilita+"']").length > 0;
         },
 
-        aggiornaListaAbilitaCivili: function ( e )
+        aggiornaListaAbilitaCivili: function ( )
         {
             var classi_selezionate = $( "#" + CIVILIAN_CLASS_BUCKET_ID ).find("li").toArray().map( function( item ){ return $( item).attr("data-id"); } ),
                 abilita_lista      = this.classInfos.abilita_civili.filter( function( item ){ return item.id_classe && classi_selezionate.indexOf( item.id_classe+"" ) !== -1; } ),
@@ -88,10 +118,12 @@
             for( var a in abilita_lista )
             {
                 abilita = abilita_lista[a];
-                if( parseInt( abilita.costo_abilita ) <= this.px_ora &&
-                    $("#"+CIVILIAN_ABILITY_BUCKET_ID).find("li[data-id='"+abilita.id_abilita+"']").length === 0 )
+                if( abilita.id_abilita && $("#"+CIVILIAN_ABILITY_BUCKET_ID).find("li[data-id='"+abilita.id_abilita+"']").length === 0 )
                 {
-                    abilita_elem = $( "<li data-id=\"" + abilita.id_abilita + "\" data-classe=\"" + abilita.id_classe + "\" data-prerequisito=\"" + abilita.prerequisito_abilita + "\" data-costo='" + abilita.costo_abilita + "'>" + abilita.nome_abilita + "</li>" );
+                    abilita_elem = $( "<li data-id=\"" + abilita.id_abilita + "\" " +
+                        "data-classe=\"" + abilita.id_classe + "\" " +
+                        "data-prerequisito=\"" + abilita.prerequisito_abilita + "\" " +
+                        "data-costo='" + abilita.costo_abilita + "'>" + abilita.nome_abilita + " ( " + abilita.costo_abilita + " PX )</li>" );
                     abilita_elem.attr("data-toggle","popover");
                     abilita_elem.attr("data-placement","top");
                     abilita_elem.attr("data-content",abilita.descrizione_abilita);
@@ -102,6 +134,12 @@
                     $("#" + CIVILIAN_ABILITY_LIST_ID).append(abilita_elem);
                 }
             }
+
+            $( "#" + CIVILIAN_ABILITY_BUCKET_ID).find("li").each(function ( i, elem)
+            {
+                if( classi_selezionate.indexOf( $( elem).attr("data-classe") ) === -1 )
+                    $( elem ).remove();
+            });
 
             this.setPopovers();
             this.setupListSelect();
@@ -146,10 +184,12 @@
             for( var a in abilita_lista )
             {
                 abilita = abilita_lista[a];
-                if( parseInt( abilita.costo_abilita ) <= this.pc_ora &&
-                    $("#"+MILITARY_ABILITY_BUCKET_ID).find("li[data-id='"+abilita.id_abilita+"']").length === 0 )
+                if( abilita.id_abilita && $("#"+MILITARY_ABILITY_BUCKET_ID).find("li[data-id='"+abilita.id_abilita+"']").length === 0 )
                 {
-                    abilita_elem = $( "<li data-id=\"" + abilita.id_abilita + "\" data-classe=\"" + abilita.id_classe + "\" data-prerequisito=\"" + abilita.prerequisito_abilita + "\" data-costo='" + abilita.costo_abilita + "'>" + abilita.nome_abilita + "</li>" );
+                    abilita_elem = $( "<li data-id=\"" + abilita.id_abilita + "\" " +
+                        "data-classe=\"" + abilita.id_classe + "\" " +
+                        "data-prerequisito=\"" + abilita.prerequisito_abilita + "\" " +
+                        "data-costo='" + abilita.costo_abilita + "'>" + abilita.nome_abilita + " ( " + abilita.costo_abilita + " PC )</li>" );
                     abilita_elem.attr("data-toggle","popover");
                     abilita_elem.attr("data-placement","top");
                     abilita_elem.attr("data-content",abilita.descrizione_abilita);
@@ -161,42 +201,15 @@
                 }
             }
 
+            $( "#" + MILITARY_ABILITY_BUCKET_ID).find("li").each(function ( i, elem)
+            {
+                if( classi_selezionate.indexOf( $( elem).attr("data-classe") ) === -1 )
+                    $( elem ).remove();
+            });
+
             this.setPopovers();
             this.setupListSelect();
             this.setupAbilityMarket();
-        },
-
-        setClassList: function ()
-        {
-            var classe       = {};
-
-            for( var cc in this.classInfos.classi_civili )
-            {
-                classe = this.classInfos.classi_civili[cc];
-                if( classe.id_classe )
-                    $("#" + CIVILIAN_CLASS_LIST_ID)
-                        .append( "<li data-id=\"" + classe.id_classe + "\">" + classe.nome_classe + "</li>" );
-            }
-
-            for( var cc in this.classInfos.classi_militari )
-            {
-                classe = this.classInfos.classi_militari[cc];
-
-                if( classe.id_classe )
-                {
-                    var disabled = classe.nome_classe.indexOf( MILITARY_ADVA_CLASS_LABEL ) !== -1 ? "disabled='disabled'" : "";
-                    $("#" + MILITARY_CLASS_LIST_ID)
-                        .append("<li data-id=\"" + classe.id_classe + "\" "+disabled+">" + classe.nome_classe + "</li>");
-                }
-            }
-
-            this.setupListSelect();
-            this.setupClassesMarket();
-        },
-
-        mostraNuovaListaClassiMilitari: function ()
-        {
-
         },
 
         setPopovers: function ()
@@ -257,13 +270,33 @@
             return true;
         },
 
+        resettaDisponibilitaClassiMilitari: function ()
+        {
+            if( $( "#"+MILITARY_CLASS_BUCKET_ID).find("li").size() === 0 )
+            {
+                $("#listaClassiMilitari").find("li:nth-child(odd)").attr("disabled", null);
+                $("#listaClassiMilitari").find("li:nth-child(even)").attr("disabled", true);
+            }
+        },
+
         elementoSelezionato: function ( elem )
         {
-            //MILITARY_BASE_CLASS_LABEL
+            $(  "#" + CIVILIAN_ABILITY_BUCKET_ID + "," +
+                "#" + CIVILIAN_ABILITY_LIST_ID + "," +
+                "#" + CIVILIAN_CLASS_BUCKET_ID + "," +
+                "#" + CIVILIAN_CLASS_LIST_ID + "," +
+                "#" + MILITARY_ABILITY_BUCKET_ID + "," +
+                "#" + MILITARY_ABILITY_LIST_ID + "," +
+                "#" + MILITARY_CLASS_BUCKET_ID + "," +
+                "#" + MILITARY_CLASS_LIST_ID
+            )
+                .not( elem.parent() )
+                .find("li")
+                .removeClass("selected");
 
-            if(elem.parent().is( $( "#listaClassiMilitari" )))
+            if( elem.parent().is( $( "#"+MILITARY_CLASS_LIST_ID ) ) )
             {
-                if (elem.parent().find(".selected").size() === 2)
+                if ( $( "#"+MILITARY_CLASS_BUCKET_ID).find("li").size() + elem.parent().find(".selected").size() === 2 )
                 {
                     elem.parent().find("li").not(".selected").attr("disabled", true);
                     return;
@@ -285,6 +318,9 @@
                     this.elementoSelezionato( elem.parent().find(".selected") );
                 }
             }
+            else if ( !elem.parent().is( $( "#"+MILITARY_CLASS_LIST_ID ) ) )
+                this.resettaDisponibilitaClassiMilitari();
+
         },
 
         setupListSelect: function ()
@@ -319,54 +355,83 @@
                 $( '#punti' + which ).addClass( "badge-danger" );
         },
 
-        calcolaCostoClassiCivili: function ( )
+        calcolaCosto: function ( selected, list_id )
         {
-            var costo = 0;
+            var costo       = 0;
 
-            $("#"+CIVILIAN_CLASS_BUCKET_ID).find("li").each(function ( i, elem )
+            if( list_id === CIVILIAN_CLASS_LIST_ID && !selected.first().attr("data-costo") )
             {
-                costo += COSTI_PROFESSIONI[i];
-                $(elem).text( $(elem).text().replace(/\(.*?\)/,"") + " ( " + COSTI_PROFESSIONI[i] + " PX )" );
-            });
+                var num_cestino  = $("#"+CIVILIAN_CLASS_BUCKET_ID).find("li").size(),
+                    costo_classe = 0;
+
+                selected.each( function( i, elem )
+                {
+                    costo_classe = COSTI_PROFESSIONI[ num_cestino + i ];
+                    $(elem).attr("data-costo", costo_classe );
+                    $(elem).text( $(elem).text().replace(/\(.*?\)/,"") + " ( " + costo_classe + " PX )" );
+                } );
+            }
+            else if ( list_id === CIVILIAN_CLASS_LIST_ID && selected.first().attr("data-costo") )
+            {
+                selected.each( function( i, elem )
+                {
+                    costo += parseInt( $(elem).attr("data-costo") );
+                    $(elem).attr("data-costo", null );
+                    $(elem).text( $(elem).text().replace(/\(.*?\)/,"")  );
+                } );
+            }
+
+            costo += selected.toArray()
+                    .map( function( item ){ return parseInt( $(item).attr("data-costo") ) || 0; } )
+                    .reduce( function( pre, curr ){ return pre + curr; } );
 
             return costo;
         },
 
         spostaNelCestino: function ( list_id, bucket_id )
         {
-            var selected = $( '#' + list_id ).find( 'li.selected' ),
-                points   = list_id.toLowerCase().indexOf("civili") !== -1 ? EXP : COM,
-                costo    = selected.toArray()
-                                   .map( function( item ){ return parseInt( $(item).attr("data-costo") ); } )
-                                   .reduce( function( pre, curr ){ console.log(pre,curr); return pre + curr; } );
-            console.log(costo);
+            var selected   = $( '#' + list_id ).find( 'li.selected' ),
+                points     = list_id.toLowerCase().indexOf("civili") !== -1 ? EXP : COM,
+                prop_punti = list_id.toLowerCase().indexOf("civili") !== -1 ? "px_ora" : "pc_ora",
+                costo      = this.calcolaCosto( selected, list_id );
+
+            if( costo > this[prop_punti] )
+            {
+                selected.each( function( i, elem )
+                {
+                    $(elem).attr("data-costo", null );
+                    $(elem).text( $(elem).text().replace(/\(.*?\)/,"")  );
+                } );
+
+                Utils.showError("Non hai abbastanza punti per comprare queste abilit&agrave;.");
+                return false;
+            }
+
             $( '[data-toggle="popover"]' ).popover( "hide" );
             selected
                 .removeClass( "selected" )
                 .appendTo( $( '#' + bucket_id ) );
 
+            this.updateRemainingPoints( points, this[prop_punti] -= costo );
 
             Utils.sortChildrenByAttribute( $( '#' + list_id ), "li", "data-id" );
             Utils.sortChildrenByAttribute( $( '#' + bucket_id ), "li", "data-id" );
-
-            if( list_id === CIVILIAN_CLASS_LIST_ID )
-            {
-                var costo = this.calcolaCostoClassiCivili();
-                this.updateRemainingPoints( points, this.px_ora -= costo );
-            }
         },
+
 		//TODO: controllare che dopo il drop i prerequisiti esistano ancora
         rimuoviDalCestino: function ( list_id, bucket_id )
         {
-            var selected = $( '#' + bucket_id ).find( 'li.selected' ),
-                points = list_id === CIVILIAN_ABILITY_LIST_ID ? EXP : COM;
+            var selected  = $( '#' + bucket_id ).find( 'li.selected' ),
+                points     = list_id.toLowerCase().indexOf("civili") !== -1 ? EXP : COM,
+                prop_punti = list_id.toLowerCase().indexOf("civili") !== -1 ? "px_ora" : "pc_ora",
+                costo      = this.calcolaCosto( selected, list_id );
 
             $( '[data-toggle="popover"]' ).popover( "hide" );
             selected
                 .removeClass( "selected" )
                 .appendTo( $( '#' + list_id ) );
 
-            //this.updateRemainingPoints( points, selected.length * 1 );
+            this.updateRemainingPoints( points, this[prop_punti] += costo );
 
             Utils.sortChildrenByAttribute( $( '#' + list_id ), "li", "data-id" );
             Utils.sortChildrenByAttribute( $( '#' + bucket_id ), "li", "data-id" );
@@ -400,6 +465,7 @@
             {
                 this.rimuoviDalCestino( MILITARY_CLASS_LIST_ID, MILITARY_CLASS_BUCKET_ID );
                 this.aggiornaListaAbilitaMilitari();
+                this.resettaDisponibilitaClassiMilitari();
             }.bind( this ) );
         },
 
@@ -434,9 +500,72 @@
             }.bind( this ) );
         },
 
+        goToMainPage: function ()
+        {
+            window.location.href = Constants.MAIN_PAGE;
+        },
+
         inviaDati: function ()
         {
+            var vuoto  = /^\s+$/,
+                errori = "";
 
+            if( !$( "#nomePG").val() || ( $( "#nomePG").val() && vuoto.test( $( "#nomePG").val() ) ) )
+                errori += "<li>Il campo nome utente non pu&ograve; essere vuoto.</li>";
+            if( $( "#" + CIVILIAN_CLASS_BUCKET_ID ).find("li").size() === 0 )
+                errori += "<li>Devi acquistare almeno una professione.</li>";
+            if( $( "#" + CIVILIAN_ABILITY_BUCKET_ID ).find("li").size() === 0 )
+                errori += "<li>Devi acquistare almeno un'abilit&agrave; civile.</li>";
+            if( $( "#" + MILITARY_CLASS_BUCKET_ID ).find("li").size() === 0 )
+                errori += "<li>Devi acquistare almeno una classe militare.</li>";
+            if( $( "#" + MILITARY_ABILITY_BUCKET_ID ).find("li").size() === 0 )
+                errori += "<li>Devi acquistare almeno un'abilit&agrave; militare.</li>";
+
+            if( errori )
+            {
+                Utils.showError( "Sono stati rilevati degli errori:<ul>" + errori + "</ul>" );
+                return false;
+            }
+
+            var classi_civili    = $( "#" + CIVILIAN_CLASS_BUCKET_ID).find("li").toArray().reduce( function( pre, curr ){ return pre + "classi_civili[]=" + $(curr).attr("data-id") + "&"; }, ""),
+                abilita_civili   = $( "#" + CIVILIAN_ABILITY_BUCKET_ID ).find("li").toArray().reduce( function( pre, curr ){ return pre + "abilita_civili[]=" + $(curr).attr("data-id") + "&"; }, ""),
+                classi_militari  = $( "#" + MILITARY_CLASS_BUCKET_ID ).find("li").toArray().reduce( function( pre, curr ){ return pre + "classi_militari[]=" + $(curr).attr("data-id") + "&"; }, ""),
+                abilita_militari = $( "#" + MILITARY_ABILITY_BUCKET_ID ).find("li").toArray().reduce( function( pre, curr ){ return pre + "abilita_militari[]=" + $(curr).attr("data-id") + "&"; }, ""),
+
+                data             = "nome=" + encodeURIComponent( $( "#nomePG").val() ) + "&" + classi_civili + abilita_civili + classi_militari + abilita_militari;
+
+            $.ajax(
+                Constants.API_POST_CREAPG,
+                {
+                    method: "POST",
+                    data: data,
+                    cache: false,
+                    //contentType: false,
+                    //processData: false,
+                    xhrFields: {
+                        withCredentials: true
+                    },
+                    success: function( data )
+                    {
+                        if ( data.status === "ok" )
+                        {
+                            $("#messageText").html("La creazione è avvenuta con successo.<br>Potrai vedere il tuo nuovo personaggio nella sezione apposita.<br>È consigliato aggiungere un Background.");
+                            $("#message").modal("show");
+                            $("#message").unbind("hidden.bs.modal");
+                            $("#message").on("hidden.bs.modal", this.goToMainPage.bind(this) );
+                        }
+                        else if ( data.status === "error" )
+                        {
+                            Utils.showError( data.message );
+                            return;
+                        }
+                    }.bind(this),
+                    error: function ( err )
+                    {
+                        Utils.showError( err );
+                    }
+                }
+            );
         }
     };
 }();
--- app/scripts/utils/Constants.js
+++ app/scripts/utils/Constants.js
@@ -5,6 +5,8 @@ Constants.SITE_URL          = "http://localhost:9000";
 
 Constants.API_POST_REGISTRA = Constants.API_URL + "/usersmanager/registra";
 Constants.API_POST_LOGIN    = Constants.API_URL + "/usersmanager/login";
+Constants.API_POST_CREAPG   = Constants.API_URL + "/charactersmanager/creapg";
+
 Constants.API_GET_LOGOUT    = Constants.API_URL + "/usersmanager/logout";
 Constants.API_GET_INFO      = Constants.API_URL + "/charactersmanager/recuperainfoclassi";
 Constants.API_GET_ALL_PGS   = Constants.API_URL + "/charactersmanager/mostratuttipersonaggi";
